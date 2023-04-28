9dd7529c19c2acc857cd9425be81d7f66af27caf
Mv0sKff <Mv0sKff@protonmail.com>
2023-04-28 08:53:33 +0200

    fix: replace quotes.rest with offline quotes

--- src/plugins/widgets/quote/api.ts
+++ src/plugins/widgets/quote/api.ts
@@ -1,4 +1,5 @@
 import { API } from "../../types";
+import { quotes } from "./quotes";
 import { Quote } from "./types";
 
 // Get developer excuse
@@ -19,33 +20,11 @@ async function getDeveloperExcuse() {
 
 // Get quote of the day
 async function getQuoteOfTheDay(category?: string) {
-  const res = await fetch(
-    "https://quotes.rest/qod.json" + (category ? `?category=${category}` : ""),
-  );
-  const body = await res.json();
-
-  if (res.status === 429) {
-    return {
-      author: body.error.message.split(".")[1] + ".",
-      quote: "Too many requests this hour.",
-    };
-  }
-
-  if (
-    body &&
-    body.contents &&
-    body.contents.quotes &&
-    body.contents.quotes[0]
-  ) {
-    return {
-      author: body.contents.quotes[0].author,
-      quote: body.contents.quotes[0].quote,
-    };
-  }
+  let quote = quotes[Math.floor(Math.random() * quotes.length)]
 
   return {
-    author: null,
-    quote: null,
+    author: quote.author,
+    quote: quote.text,
   };
 }
 
